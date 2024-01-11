8186af45a559eb442775dfe09504fd16ca41a206
Dimas Ihsan Pratama <118706487+Dimasinst@users.noreply.github.com>
2024-01-11 15:23:35 +0700

    Add files via upload

---
+++ tugasphp/index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<h1>Media Online</h1>
+    <h2>Sosial Media Developer</h2>
+    <p>Belajar dan berbagii agar hidup menjadi lebih baik</p>
+    <h2>Benefit Join di Media Online</h2>
+         <li>Mendapatkan motivasi dari sasama para developer</li>
+         <li>Sharing kknowlenge</li>
+         <li>Dibuat oleh calon web developer terbaik</li>
+    <h2>Cara Bergabung ke Media Online</h2>
+         <li>Mengunjungi Website ini</li>
+         <li>Mendaftarkan di form <a href="form.php">Form Sign UP</a> </li>
+         <li>Selesai</li>
+</body>
+</html>
